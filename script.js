9cc60a2e87c9662048c6e7658b2bf4ba6ede6022
Seband54 <111619206+Seband54@users.noreply.github.com>
2025-04-27 11:22:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ let modelo = null;
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let resultado = document.getElementById("resultado");
+let grosorInput = document.getElementById("grosor");
 
-// Cargar el modelo preentrenado
+ctx.lineWidth = grosorInput.value;
+ctx.lineCap = "round";
+ctx.strokeStyle = "#000";
+
+grosorInput.addEventListener("input", () => {
+    ctx.lineWidth = grosorInput.value;
+});
+
 (async () => {
     console.log("Cargando modelo...");
-    const model = await tf.loadLayersModel('model.json');
-    model.build([null, 28, 28, 1]); 
+    modelo = await tf.loadLayersModel('model.json');
+    modelo.build([null, 28, 28, 1]);
 })();
 
-// Función para limpiar el canvas
-function limpiarCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    resultado.innerHTML = '';
-}
-
-// Función para dibujar en el canvas
 canvas.addEventListener("mousedown", function (e) {
     ctx.beginPath();
     ctx.moveTo(e.offsetX, e.offsetY);
@@ -25,27 +26,29 @@ canvas.addEventListener("mousedown", function (e) {
 canvas.addEventListener("mouseup", function () {
     canvas.removeEventListener("mousemove", dibujar);
 });
+
 function dibujar(e) {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 }
 
-// Función para predecir la prenda
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    resultado.innerHTML = '';
+}
+
 async function predecir() {
     if (modelo !== null) {
-        // Preprocesar el canvas para que sea adecuado para el modelo
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         let tensor = tf.browser.fromPixels(imageData).mean(2).toFloat().expandDims(0).expandDims(-1);
-        tensor = tensor.div(255.0); // Normaliza los valores de los píxeles
+        tensor = tensor.div(255.0);
 
-        // Realizar la predicción
         let prediccion = await modelo.predict(tensor).data();
         let clase = prediccion.indexOf(Math.max(...prediccion));
-
-        // Mostrar el resultado
         resultado.innerHTML = `Predicción: ${clase}`;
     } else {
         resultado.innerHTML = "Cargando el modelo, intenta de nuevo...";
     }
 }
+
 
